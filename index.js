528b2361a730852aacda1ecd21520df4a121dc6c
Atharv <atharvbhandare2001@gmail.com>
2021-09-10 17:47:52 +0530

    Parsed middleware

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser')
+app.use(cookieParser());
+
 app.get('/greet', (req, res) => {
-    res.send('hello there')
+    const { name = 'Joaquin' } = req.cookies;
+    res.send(`Hey there ${name}`);
 })
 
 app.get('/cookies', (req, res) => {
     res.cookie('name', 'Atharv Bhandare')
+    res.cookie('animal', 'harlequin shrimp')
     res.send('Sent you a cookie')
 })
 
