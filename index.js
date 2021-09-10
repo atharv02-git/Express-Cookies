bc8af6f37ec6b492b06c084644ca9aba3b76dd69
Atharv <atharvbhandare2001@gmail.com>
2021-09-10 18:41:34 +0530

    Signed Cookie

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser')
+app.use(cookieParser('secretCookie'));
+
 app.get('/greet', (req, res) => {
-    res.send('hello there')
+    const { name = 'Joaquin' } = req.cookies;
+    res.send(`Hey there ${name}`);
 })
 
 app.get('/cookies', (req, res) => {
     res.cookie('name', 'Atharv Bhandare')
+    res.cookie('animal', 'harlequin shrimp')
     res.send('Sent you a cookie')
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true })
+    res.send('Sent your signed cookies')
+})
+
+app.get('/verifySigned', (req, res) => {
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+    res.send(req.signedCookies)
 })
 
 app.listen(3000, (req, res) => {
